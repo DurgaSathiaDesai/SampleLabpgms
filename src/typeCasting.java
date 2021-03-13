eb1b82d80ba55d2fad16471249081840085f91c5
Durga <durga.nskumar@gmail.com>
2021-03-13 16:23:40 +0000

    Initial version of pgm for typecasting

---
+++ src/typeCasting.java
@@ -0,0 +1,17 @@
+
+public class typeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ char c = 'A';
+ int num = c;
+ System.out.println(num);
+ long l =c;
+ System.out.println(l);
+ 
+ double d = 50.5;
+ int i = (int)d;
+ System.out.println(i);
+	}
+
+}
